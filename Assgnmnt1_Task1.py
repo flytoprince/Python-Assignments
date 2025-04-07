3daf1c78f404b5dffc0ed9f840c548ae9a9e2aa1
flytoprince <flytoprince@gmail.com>
2025-04-08 00:55:18 +0530

    Add files via upload

---
+++ Assgnmnt1_Task1.py
@@ -0,0 +1,12 @@
+# ASSIGNMENT-1 " Basic Python Concepts "
+
+a=int(input("Enter first number :"))
+b=int(input("Enter second number :"))
+add=int(a+b)
+sub=int(a-b)
+mult=int(a*b)
+div=float(a/b)
+print("Addition : ", add)
+print("Subtraction : ", sub)
+print("Multiplication : ", mult)
+print("Division : ", div)
---
+++ Assignmnt1_Task2.py
@@ -0,0 +1,5 @@
+# ASSIGNMENT 1 Task-2
+firstname= (input("Enter first name: "))
+lastname= (input("Enter last name: "))
+fullname=firstname+" "+lastname
+print(f"Hello, {fullname} ! Welcome to the Python class !")
